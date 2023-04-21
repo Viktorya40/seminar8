0d9ac2369522bfb3ac071e893e94733ae5311432
Viktorya <79201287827@yandex.ru>
2023-04-22 00:05:23 +0300

    Задача 58

--- Program.cs
+++ Program.cs
@@ -1,54 +1,68 @@
-﻿Console.Write("Введите размер m массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите размер n массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] randomArray = new int[m,n];
+﻿Console.Clear();
+Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений: ");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
 
-void mas(int m, int n)
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
 {
-int i,j;
-Random rand = new Random();
-for (i = 0; i < m; i++)
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
 {
-for (j = 0; j < n; j++)
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
 {
-randomArray[i,j] = rand.Next(1,9);
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
 }
-}
-}
-void printm(int[,] array)
+
+void WriteArray (int[,] array)
 {
-int i,j;
-for (i = 0; i < array.GetLength(0); i++)
-{
-Console.WriteLine();
-for (j = 0; j < array.GetLength(1); j++)
-{
-Console.Write($"{array[i,j]} ");
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
 }
-Console.WriteLine();
-}
-}
-mas(m,n);
-Console.WriteLine("\nИсходный массив: ");
-printm(randomArray);
-int SumLine(int[,] array, int i)
-{
-int sum = array[i,0];
-for (int j = 1; j < array.GetLength(1); j++)
-{
-sum += array[i,j];
-}
-return sum;
-}
-int minSum = 1;
-int sum = SumLine(randomArray, 0);
-for (int i = 1; i < randomArray.GetLength(0); i++)
-{
-if (sum > SumLine(randomArray, i))
-{
-sum = SumLine(randomArray, i);
-minSum = i+1;
-}
-}
-Console.WriteLine($"\nСтрока c наименьшей суммой элементов: {minSum}");
